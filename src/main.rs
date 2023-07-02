f741ee76a791d2dfb46043ca14202a615d18acef
Ashish Binu <ashishbinu90@gmail.com>
2023-07-02 13:11:15 +0530

    feat(repl): add CTRL+D to exit

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 mod lexer;
 mod repl;
 
+use std::io;
+
 use crate::repl::repl::start;
 
 fn main() {
     println!("Monkey Interpreter");
-    start();
+    let mut stdout = io::stdout();
+    let stdin = io::stdin();
+    start(&mut stdin.lock(), &mut stdout);
 }
--- src/repl/repl.rs
+++ src/repl/repl.rs
@@ -1,24 +1,34 @@
-use std::io::{self, Write};
+use std::io::{BufRead, Write};
 
 use crate::lexer::{lexer::Lexer, token::Token};
 
 const PROMPT: &str = ">> ";
 
-pub fn start() {
+pub fn start(stdin: &mut dyn BufRead, stdout: &mut dyn Write) {
     loop {
-        print!("{}", PROMPT);
-        io::stdout().flush().unwrap();
+        write!(stdout, "{}", PROMPT).unwrap();
+        stdout.flush().unwrap();
 
         let mut input = String::new();
-        io::stdin().read_line(&mut input).unwrap();
-        let mut lexer = Lexer::new(input);
-        loop {
-            let token = lexer.next_token();
-            if token == Token::Eof {
+        match stdin.read_line(&mut input) {
+            Ok(0) => {
                 break;
             }
-            print!("{:?} ", token);
+            Ok(_) => {
+                let mut lexer = Lexer::new(input);
+                loop {
+                    let token = lexer.next_token();
+                    if token == Token::Eof {
+                        break;
+                    }
+                    write!(stdout, "{:?} ", token).unwrap();
+                }
+                writeln!(stdout).unwrap();
+            }
+            Err(e) => {
+                eprintln!("Error: {}", e);
+                break;
+            }
         }
-        println!();
     }
 }
